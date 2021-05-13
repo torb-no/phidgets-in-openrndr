04aebae5553b36b221861e9937db6de2605676f9
Torb Lunde Miranda <1179046+torb-xyz@users.noreply.github.com>
2021-05-13 13:36:34 +0200

    Simple digital input example (could work with button)

---
+++ src/main/kotlin/DigitalInputExample.kt
@@ -0,0 +1,36 @@
+import com.phidget22.DigitalInput
+import com.phidget22.VoltageRatioInput
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+
+fun main() = application {
+    configure {
+        width = 768
+        height = 576
+    }
+
+    program {
+
+        var state = false
+        with (DigitalInput()) {
+            hubPort = 0
+
+            addStateChangeListener {
+                state = it.state
+                println("DigitalInput state changed to ${it.state}")
+            }
+
+            open()
+            ended.listen { close() }
+        }
+
+        extend {
+            if (state) {
+                drawer.clear(ColorRGBa.GREEN)
+            } else {
+                drawer.clear(ColorRGBa.RED)
+            }
+
+        }
+    }
+}
