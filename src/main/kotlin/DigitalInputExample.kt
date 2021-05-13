7aa38b2ec9e635dd8633aa0c661ba83ae67e2579
Torb Lunde Miranda <1179046+torb-xyz@users.noreply.github.com>
2021-05-13 13:40:37 +0200

    Explanatory comment

--- src/main/kotlin/DigitalInputExample.kt
+++ src/main/kotlin/DigitalInputExample.kt
@@ -14,6 +14,10 @@ fun main() = application {
             hubPort = 0
 
             addStateChangeListener {
+                // Note: event handle callback is run on
+                // Phidget library's own thread, so you can't run
+                // or call to drawing code here unless you wrap it in
+                // launch {} clause to get back to the normal OPENRNDR thread
                 state = it.state
                 println("DigitalInput state changed to ${it.state}")
             }
