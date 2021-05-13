07aa007ca4149192927da4cef1a9b1128178ec16
Torb Lunde Miranda <1179046+torb-xyz@users.noreply.github.com>
2021-05-13 13:24:56 +0200

    First simple example program

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,10 +1,5 @@
+import com.phidget22.VoltageRatioInput
 import org.openrndr.application
-import org.openrndr.color.ColorRGBa
-import org.openrndr.draw.loadFont
-import org.openrndr.draw.loadImage
-import org.openrndr.draw.tint
-import kotlin.math.cos
-import kotlin.math.sin
 
 fun main() = application {
     configure {
@@ -13,19 +8,29 @@ fun main() = application {
     }
 
     program {
-        val image = loadImage("data/images/pm5544.png")
-        val font = loadFont("data/fonts/default.otf", 64.0)
+        var value = 0.0
+        with (VoltageRatioInput()) {
+            // Configure settings depending on what it connected where
+            isHubPortDevice = true
+            hubPort = 0
+
+            // Configuration required to set after attach
+            addAttachListener {
+                dataInterval = 25
+            }
+
+            // Use event to get value
+            addVoltageRatioChangeListener {
+                value = it.voltageRatio
+            }
+
+            open()
+            ended.listen { close() }
+        }
 
         extend {
-            drawer.drawStyle.colorMatrix = tint(ColorRGBa.WHITE.shade(0.2))
-            drawer.image(image)
-
-            drawer.fill = ColorRGBa.PINK
-            drawer.circle(cos(seconds) * width / 2.0 + width / 2.0, sin(0.5 * seconds) * height / 2.0 + height / 2.0, 140.0)
-
-            drawer.fontMap = font
-            drawer.fill = ColorRGBa.WHITE
-            drawer.text("OPENRNDR", width / 2.0, height / 2.0)
+            val x = value * width
+            drawer.circle(x, 20.0, 5.0)
         }
     }
 }
